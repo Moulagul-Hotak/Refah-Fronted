ee07a0da61014d79331bb387bab76e8acac5bac4
Moulagul Hotak <moulagul_hotak@yahoo.com>
2023-03-29 13:53:55 +0430

    create new user without image resizing done

--- src/User/CreateNewUser.jsx
+++ src/User/CreateNewUser.jsx
@@ -1,32 +1,62 @@
-import { type } from "@testing-library/user-event/dist/type";
 import axios from "axios";
-import React, { useState } from "react";
+import { useFormik } from "formik";
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import { useNavigate } from "react-router-dom";
 
 const API_URL = "http://localhost:8000/api/users";
+const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
 
 function CreateNewUser() {
 
-    const [CreateNewUserForm, setCreateNewUserFormValue] = useState({ name: '', email: '', password: '', confirm_password: '' });
-    const { register, formState: { errors }, handleSubmit } = useForm();
-    const handleFormSubmit = () => {
-        // e.preventDefault();
-        axios.post(
-            API_URL,
-            CreateNewUserForm)
-            .then((response) => {
-                console.log(response);
-            }).catch((error) => {
-                if (error.response.data.code === 400) {
-                    console.log(error.response.data.error);
-                }
-            });
-    }
+    const navigate = useNavigate();
 
-    const handleInput = (e) => {
-        const { name, value } = e.target;
-        setCreateNewUserFormValue({ ...CreateNewUserForm, [name]: value })
-    }
+    const formik = useFormik({
+        initialValues: {
+            image: ""
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('Name is required'),
+            email: Yup.string()
+                .required('Email is required')
+                .email('Enter a correct email'),
+            image: Yup.mixed()
+                .required("Image is Required")
+                .test(
+                    "FILE_SIZE",
+                    "Your image is too big :(",
+                    (value) => !value || (value && value.size <= 1024 * 1024)
+                )
+                .test(
+                    "FILE_TYPE",
+                    "Please select image :(",
+                    (value) => !value || (value && SUPPORTED_FORMATS.includes(value?.type))
+                ),
+            password: Yup.string()
+                .required('Password is required')
+                .min(5, 'Password must be at least 5 char long'),
+            confirm_password: Yup.string()
+                .required('Password is mendatory')
+                .oneOf([Yup.ref('password')], 'Passwords does not match'),
+        }),
+        onSubmit: async () => {
+            const formData = new FormData(document.getElementById('newUserForm'));
+            console.log(formData);
+            axios.post(
+                API_URL,
+                formData)
+                .then((response) => {
+                    navigate('/');
+                }).catch((error) => {
+                    if (error.response.data.code === 400) {
+                        console.log(error.response.data.error);
+                    }
+                });
+        }
+    })
+
     return <>
         <div className="col-lg-6 mx-auto">
             <div className="card">
@@ -37,34 +67,43 @@ function CreateNewUser() {
                     <button className="btn btn-outline-primary">Back</button>
                 </div>
                 <div className="card-body">
-                    <form action={API_URL} method="post" onSubmit={handleSubmit(handleFormSubmit)}>
+                    <form id="newUserForm" onSubmit={formik.handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="name">Name: </label>
-                            <input type="text" className="form-control" name="name" {...register("name", { required: true })} />
+                            <input type="text" className="form-control" name="name" onChange={(e) => { formik.setFieldValue('name', e.target.value) }} />
                             <span className="text-danger">
-                                {errors.name?.type === "required" && "Name is Required"}
+                                {formik.errors.name && (
+                                    <span className="text-danger">{formik.errors.name}</span>
+                                )}
                             </span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email: </label>
-                            <input type="text" className="form-control" {...register("email", { required: true })} name="email" />
-                            <span className="text-danger">
-                                {errors.email?.type === "required" && "Email is Required"}
-                            </span>
+                            <input type="text" className="form-control" onChange={(e) => { formik.setFieldValue('email', e.target.value) }} name="email" />
+                            {formik.errors.email && (
+                                <span className="text-danger">{formik.errors.email}</span>
+                            )}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="image">Image: </label>
+                            <input type="file" className="form-control" onChange={(e) => { formik.setFieldValue('image', e.target.files[0]) }} name="image" />
+                            {formik.errors.image && (
+                                <span className="text-danger">{formik.errors.image}</span>
+                            )}
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password: </label>
-                            <input type="password" className="form-control" {...register("password", { required: true })} name="password" />
-                            <span className="text-danger">
-                                {errors.password?.type === "required" && "Password is Required"}
-                            </span>
+                            <input type="password" className="form-control" onChange={(e) => { formik.setFieldValue('password', e.target.value) }} name="password" />
+                            {formik.errors.password && (
+                                <span className="text-danger">{formik.errors.password}</span>
+                            )}
                         </div>
                         <div className="form-group">
                             <label htmlFor="confirm_password">Confirm Password: </label>
-                            <input type="password" className="form-control" {...register("confirm_password", { required: true })} name="confirm_password" />
-                            <span className="text-danger">
-                                {errors.confirm_password?.type === "required" && "Confirm Password is Required"}
-                            </span>
+                            <input type="password" className="form-control" onChange={(e) => { formik.setFieldValue('confirm_password', e.target.value) }} name="confirm_password" />
+                            {formik.errors.confirm_password && (
+                                <span className="text-danger">{formik.errors.confirm_password}</span>
+                            )}
                         </div>
                         <button type="submit" className="btn btn-outline-success">Save</button>
                     </form>
