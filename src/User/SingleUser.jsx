2ad9b052856e801546a4be4aad70374de9c2a5ee
Moulagul Hotak <moulagul_hotak@yahoo.com>
2023-03-29 14:02:39 +0430

    user delete done

--- src/User/SingleUser.jsx
+++ src/User/SingleUser.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const API_URL = "http://localhost:8000/api/users/";
 function SingleUser(singleUser) {
