356a9d4ac725148d70564e825a1bc3e21d842e80
Moulagul Hotak <moulagul_hotak@yahoo.com>
2023-03-29 12:01:56 +0430

    before yup and password confirmation matcing

--- src/User/CreateNewUser.jsx
+++ src/User/CreateNewUser.jsx
@@ -1,14 +1,16 @@
+import { type } from "@testing-library/user-event/dist/type";
 import axios from "axios";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const API_URL = "http://localhost:8000/api/users";
 
 function CreateNewUser() {
 
     const [CreateNewUserForm, setCreateNewUserFormValue] = useState({ name: '', email: '', password: '', confirm_password: '' });
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const handleFormSubmit = () => {
+        // e.preventDefault();
         axios.post(
             API_URL,
             CreateNewUserForm)
@@ -35,22 +37,34 @@ function CreateNewUser() {
                     <button className="btn btn-outline-primary">Back</button>
                 </div>
                 <div className="card-body">
-                    <form action={API_URL} method="post" onSubmit={handleFormSubmit}>
+                    <form action={API_URL} method="post" onSubmit={handleSubmit(handleFormSubmit)}>
                         <div className="form-group">
                             <label htmlFor="name">Name: </label>
-                            <input type="text" className="form-control" value={CreateNewUserForm.name} onChange={handleInput} name="name" />
+                            <input type="text" className="form-control" name="name" {...register("name", { required: true })} />
+                            <span className="text-danger">
+                                {errors.name?.type === "required" && "Name is Required"}
+                            </span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email: </label>
-                            <input type="email" className="form-control" value={CreateNewUserForm.email} onChange={handleInput} name="email" />
+                            <input type="text" className="form-control" {...register("email", { required: true })} name="email" />
+                            <span className="text-danger">
+                                {errors.email?.type === "required" && "Email is Required"}
+                            </span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password: </label>
-                            <input type="password" className="form-control" value={CreateNewUserForm.password} onChange={handleInput} name="password" />
+                            <input type="password" className="form-control" {...register("password", { required: true })} name="password" />
+                            <span className="text-danger">
+                                {errors.password?.type === "required" && "Password is Required"}
+                            </span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="confirm_password">Confirm Password: </label>
-                            <input type="password" className="form-control" value={CreateNewUserForm.confirm_password} onChange={handleInput} name="confirm_password" />
+                            <input type="password" className="form-control" {...register("confirm_password", { required: true })} name="confirm_password" />
+                            <span className="text-danger">
+                                {errors.confirm_password?.type === "required" && "Confirm Password is Required"}
+                            </span>
                         </div>
                         <button type="submit" className="btn btn-outline-success">Save</button>
                     </form>
