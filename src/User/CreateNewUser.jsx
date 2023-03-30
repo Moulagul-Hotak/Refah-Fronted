2e7d7a5cdb429be48d547d9889dafa88343ac1dd
Moulagul Hotak <moulagul_hotak@yahoo.com>
2023-03-30 10:28:48 +0430

    image resized

--- src/User/CreateNewUser.jsx
+++ src/User/CreateNewUser.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useFormik } from "formik";
 import * as Yup from 'yup'
 import { useNavigate } from "react-router-dom";
+import Resizer from "react-image-file-resizer";
+import { useState } from "react";
 
 const API_URL = "http://localhost:8000/api/users";
 const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
@@ -9,6 +11,7 @@ const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
 function CreateNewUser() {
 
     const navigate = useNavigate();
+    const [img, setImg] = useState("");
 
     const formik = useFormik({
         initialValues: {
@@ -41,7 +44,7 @@ function CreateNewUser() {
         }),
         onSubmit: async () => {
             const formData = new FormData(document.getElementById('newUserForm'));
-            console.log(formData);
+            formData.set('image', img);
             axios.post(
                 API_URL,
                 formData)
@@ -49,11 +52,29 @@ function CreateNewUser() {
                     navigate('/');
                 }).catch((error) => {
                     if (error.response.data.code === 400) {
-                        console.log(error.response.data.error);
+                        // console.log(error.response.data.error);
                     }
                 });
         }
     })
+
+    const imageResizer = (image) => {
+        return new Promise((resolve) => {
+            Resizer.imageFileResizer(
+                image,
+                400, // New width
+                400, // New height
+                'JPG', // Output format
+                100, // Quality
+                0, // Rotation
+                (uri) => {
+                    setImg(uri);
+                },
+                'blob' // Output type
+                , 200, 200
+            );
+        });
+    };
 
     return <>
         <div className="col-lg-6 mx-auto">
@@ -84,7 +105,7 @@ function CreateNewUser() {
                         </div>
                         <div className="form-group">
                             <label htmlFor="image">Image: </label>
-                            <input type="file" className="form-control" onChange={(e) => { formik.setFieldValue('image', e.target.files[0]) }} name="image" />
+                            <input type="file" className="form-control" onChange={(e) => { imageResizer(e.target.files[0]); formik.setFieldValue('image', e.target.files[0]) }} name="image" id="image" />
                             {formik.errors.image && (
                                 <span className="text-danger">{formik.errors.image}</span>
                             )}
